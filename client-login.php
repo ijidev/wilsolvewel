67eb4104a89c92e8d5cdde7100a9cbe1b82cd292
ijidev <iheanyichukwui94@gmail.com>
2026-05-17 16:07:39 +0100

    refactor: update redirect logic and enforce client-side caching headers

--- client-login.php
+++ client-login.php
@@ -1,5 +1,6 @@
 <?php
-$redirect = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/client/login.php';
-header('HTTP/1.1 301 Moved Permanently');
-header("Location: $redirect");
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
+header('Location: /client/login.php', true, 302);
 exit;
